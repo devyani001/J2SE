527200d50713ba6bc76f19d9123c045c3c58fd88
devyani001 <devyani140424@gmail.com>
2025-07-28 15:48:30 +0530

    day-1 introduction to Full Stack Development - ROADMAP

---
+++ 01/introduction.java
@@ -0,0 +1,27 @@
+            // J2SE - CORE JAVA
+
+/*
+    java programming / Core Java ==> J2SE
+
+    Q. Sir, i want to become a java developer (fullstack developer)
+    =====================================================================
+
+    UI Developer                    Backend Developer                DB Developer
+    =============                   ==================               ==================
+    FRONT-END                       BACK-END                         DATABASE
+    ==========                      ==========                       ==========
+    UI Technologies                 Java developer                   SQL Developer
+
+    - HTML                          - Core Java (J2SE)               Query Languages
+    - CSS                           - Advance Java (J2EE)               - SQL
+    - Bootstrap                     - Hibernate                           ex :- MySQL, PostgreSQL
+    - JavaScript                    - Spring Framework                  - NoSQL
+                                    - Spring Boot                         ex :- MongoDB
+    - React/Angular                 - Microservices
+
+                                    - DSA Using Java
+                                    - Design Patterns
+
+
+===================================================================================================== 
+*/
